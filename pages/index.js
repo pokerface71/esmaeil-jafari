362511aeadc82ab32067423cb22944a9d30efeb8
esmaeiljafari1992 <esmaeiljafari1992@gmail.com>
2022-02-19 12:42:33 -0800

    config

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="container">
+			<Head>
+				<title>Next.js Starter!</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+			<main>
+				<Header title="Welcome to my app!" />
+				<div className="text-3xl font-bold underline"> Esmaeil jafari</div>
+			</main>
 
-      <Footer />
-    </div>
-  )
+			<Footer />
+		</div>
+	);
 }
