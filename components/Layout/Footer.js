3d78fde4e6d4ff83dc29aacb17cc825039f0cc7e
pokerface71 <esmaeiljafari1992@gmail.com>
2022-02-22 06:23:28 -0800

    refactor

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<>
-			<footer className="flex items-center justify-center w-full mt-52">
+			<footer className="flex items-center justify-center w-full mt-52 ">
 				front end developer <img src="/netliheart.svg" alt="Netlify Logo" /> for
 				ever (esmaeil jafari)
 			</footer>
